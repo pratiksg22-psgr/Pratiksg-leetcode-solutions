8c6099a9a3eefcfcd7fe79750b4c240000744de7
pratiksg22-psgr <pratiksg22@gmail.com>
2026-09-10 22:39:58 +0530

    Create sqrt x.java

---
+++ sqrt x.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+
+        int left = 1;
+        int right = x / 2;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Use division to avoid mid * mid overflow
+            if (mid <= x / mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return right;
+    }
+}
